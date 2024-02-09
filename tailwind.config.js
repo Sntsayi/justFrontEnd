e1b680b72115e3e9e2537d0a8d144a35e690a880
TahaSahraei <taha23sahraii@gmail.com>
2024-02-09 22:57:26 +0330

    add Login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./build/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        myFont: ["Noto Serif", "serif"],
+      },
+      colors: {
+        "dark-gray": "#333333",
+        teal: "#008080",
+        bluewhte: "#0358a8",
+        semiWhite: "#ecf0f1",
+      },
+      backgroundImage: {
+        "gradinatDark-gray":
+          "linear-gradient(109deg, rgba(85,94,99,1) 0%, rgba(26,26,48,1) 67%, rgba(19,37,41,1) 100%)",
+      },
+      margin: {
+        "50%": "20%",
+      },
+      spacing: {
+        70: "17.5rem",
+        "94%": "94%",
+      },
+    },
   },
   plugins: [],
 };
